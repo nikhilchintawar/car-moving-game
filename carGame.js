16f8c31c5572d6ac5f89a6bcfe45802760775217
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2020-11-23 13:01:18 +0530

    optimized the code for all the cases and added the google font

--- carGame.js
+++ carGame.js
@@ -1,7 +1,7 @@
 const startButton = document.querySelector(".startButton");
 const refreshButton = document.querySelector(".refreshButton");
-
-
+const modalContainer = document.querySelector(".modal-container");
+const resultText = document.querySelector(".result");
 
 let randomPoints = [20, 40, 60 ,80 ,100];
 let petrolPumpPoints = [];
@@ -12,12 +12,12 @@ let randomMovePoint = 0;
 let lastRandomMovePoint = 0;
 let move = 0;
 
+//gives the random numbers in range.
 const getRandomPoint = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-
-const carGame = (event) => {
+const startCarGame = () => {
     let div = document.createElement("div");
     let startText = document.createTextNode("Game Started");
     div.appendChild(startText);
@@ -35,42 +35,25 @@ const carGame = (event) => {
     petrolPumpPoints.sort((a, b) => a-b)
     petrolPumpPointsText.textContent = `Petrol pumps generated at ${petrolPumpPoints}`;
     div.appendChild(petrolPumpPointsText);
-    
-    let newDiv = document.createElement("div");
-    document.body.appendChild(newDiv);
-    newDiv.classList.add("results")
-
-    for(let i = 0; i < endPoint; i++){
-        moveCar();
-        if(randomMovePoint >= 100){
-            let moveText = document.createElement("p");
-            newDiv.appendChild(moveText);
-            moveText.textContent = `Move ${move} - Car at ${randomMovePoint}, petrol remaining ${petrolRemaining}. destination reached.`;
-            return;
-        }
-
-        if(petrolRemaining <= 6 && (randomMovePoint - lastRandomMovePoint) <= 6){
-            let moveText = document.createElement("p");
-            newDiv.appendChild(moveText);
-            moveText.textContent = `Move ${move} - Car at ${randomMovePoint}, petrol remaining ${petrolRemaining}`;
-            
-            if(petrolRemaining > 0){
-                randomMovePoint = randomMovePoint + petrolRemaining;
-                petrolRemaining = 0;
-                move++;
-                let moveText = document.createElement("p");
-                newDiv.appendChild(moveText);
-                moveText.textContent = `Move ${move} - Car at ${randomMovePoint}, petrol remaining ${petrolRemaining}, Game Over.`;
-            }
-            return
-        }
-        let moveText = document.createElement("p");
-        newDiv.appendChild(moveText);
-        moveText.textContent = `Move ${move} - Car at ${randomMovePoint}, petrol remaining ${petrolRemaining}`;
-    }
 }
 
+// creates the sentence after every move
+const createMoveText = (resultsDiv, text) => {
+    let moveText = document.createElement("p");
+    resultsDiv.appendChild(moveText);
+    moveText.textContent = text;
+}
 
+//fills the petrol if reached at petrol point.
+const fillPetrol = () => {
+    if(petrolRemaining <= 10){
+        petrolPumpPoints.map(petrolPumpPoint => {
+            if(lastRandomMovePoint <= petrolPumpPoint && petrolPumpPoint <= randomMovePoint){
+                petrolRemaining = petrolRemaining + 20;
+            }
+        })
+    }
+}
 
 const moveCar = () => {
     let min = randomMovePoint;
@@ -78,20 +61,40 @@ const moveCar = () => {
     randomMovePoint = getRandomPoint(min, max);
     move++;
     petrolRemaining = petrolRemaining - (randomMovePoint - lastRandomMovePoint);
-
-    if(petrolRemaining <= 10){
-        petrolPumpPoints.map(petrolPumpPoint => {
-            if(lastRandomMovePoint < petrolPumpPoint && petrolPumpPoint < randomMovePoint){
-                petrolRemaining = petrolRemaining + 20;
-            }
-        })
-    }
+    fillPetrol();
     lastRandomMovePoint = randomMovePoint;
 }
+
+const carGame = (event) => {
+    startCarGame();
+    let resultsDiv = document.createElement("div");
+    document.body.appendChild(resultsDiv);
+    resultsDiv.classList.add("results")
+
+    while(randomMovePoint < 100){
+        moveCar();
+        if(randomMovePoint >= 100){
+            let destinationText = `Move ${move} - Car at ${randomMovePoint}, petrol remaining ${petrolRemaining}. destination reached.`
+            createMoveText(resultsDiv, destinationText)
+            return;
+        }
+        if(petrolRemaining <= 0 && !petrolPumpPoints.includes(randomMovePoint)){
+            randomMovePoint = randomMovePoint + petrolRemaining;
+            petrolRemaining = 0;
+            let gameOverText = `Move ${move} - Car at ${randomMovePoint}, petrol remaining ${petrolRemaining}, Game Over.`;
+            createMoveText(resultsDiv, gameOverText);
+            return;
+        }
+        let movingText = `Move ${move} - Car at ${randomMovePoint}, petrol remaining ${petrolRemaining}`;
+        createMoveText(resultsDiv, movingText);
+    }
+}
+
 
 const refreshPage = () => {
     location.reload()
 }
+
 
 startButton.addEventListener("click", carGame);
 refreshButton.addEventListener("click", refreshPage);
